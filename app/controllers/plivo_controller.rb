3efb81b00d7a217be5a478aa89d6c0b27da34d81
GarPit <garik.piton@gmail.com>
2013-09-05 15:09:45 +0400

    add nokogiri

--- app/controllers/plivo_controller.rb
+++ app/controllers/plivo_controller.rb
@@ -1,10 +1,22 @@
 class PlivoController < ApplicationController
     def answer
-        puts params
+        #puts params
+        builder = Nokogiri::XML::Builder.new do |xml|
+            xml.Response {
+                xml.Play "https://s3.amazonaws.com/plivocloud/Trumpet.mp3"
+            }
+        end
+        render :xml => builder
     end
 
     def hangup
-        puts params
+        #puts params
+        builder = Nokogiri::XML::Builder.new do |xml|
+            xml.Response {
+                xml.Play "https://s3.amazonaws.com/plivocloud/Trumpet.mp3"
+            }
+        end
+        render :xml => builder
     end
 end
 
