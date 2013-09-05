b78075042f498a3b506a4f12e7d95fda0aac217f
GarPit <garik.piton@gmail.com>
2013-09-05 16:25:13 +0400

    add Dial tag to plivo response

--- app/controllers/plivo_controller.rb
+++ app/controllers/plivo_controller.rb
@@ -3,7 +3,10 @@ class PlivoController < ApplicationController
         #puts params
         builder = Nokogiri::XML::Builder.new do |xml|
             xml.Response {
-                xml.Play "https://s3.amazonaws.com/plivocloud/Trumpet.mp3"
+                #xml.Play "https://s3.amazonaws.com/plivocloud/Trumpet.mp3"
+                xml.Dial {
+                    xml.Number params["To"]
+                }
             }
         end
         render :xml => builder
@@ -13,7 +16,8 @@ class PlivoController < ApplicationController
         #puts params
         builder = Nokogiri::XML::Builder.new do |xml|
             xml.Response {
-                xml.Play "https://s3.amazonaws.com/plivocloud/Trumpet.mp3"
+                #xml.Play "https://s3.amazonaws.com/plivocloud/Trumpet.mp3"
+                xml.Hangup
             }
         end
         render :xml => builder
